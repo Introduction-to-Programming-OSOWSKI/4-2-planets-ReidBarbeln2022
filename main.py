2c6fd74ece25cd72889327e41bc38d91e33ab312
ReidBarbeln2022 <814684@mystma.org>
2021-02-18 01:04:26 +0000

    planets try 1

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def planets(p):
     placment = "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"
     for i in range (0, len (placment)):
         if p == placment [i]:
-            return i
+            return i + 1
 
-    
+    return "russia is not a planet"
     
     
     
     return placment [p]
 
-print (planets("earth"))
+print (planets("mercury"))
